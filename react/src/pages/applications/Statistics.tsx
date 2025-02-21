b3d26d3fb503f5ea6af136bd43884af6ae1ae215
Vladimir Elisavetski <yvova88@yahoo.com>
2025-02-21 10:24:42 -0700

    statistics -> go to applicaiton

--- react/src/pages/applications/Statistics.tsx
+++ react/src/pages/applications/Statistics.tsx
@@ -9,9 +9,10 @@ import {
 } from './utils';
 import CollapseContainer from '../../components/CollapseContainer';
 import { IApplication } from '../../services/applications';
-import { LANG, STATUS_MAP } from '../../utils/constants';
+import { LANG, ROUTES, STATUS_MAP } from '../../utils/constants';
 import Separtor from './Separator';
 import GoThereLink from '../../components/GoThereLink';
+import { useLocation } from 'wouter';
 
 interface StatisticsProps {
     applications: IApplication[];
@@ -19,7 +20,11 @@ interface StatisticsProps {
 
 const Statistics: React.FC<StatisticsProps> = ({ applications }) => {
     const [isOpen, setIsOpen] = useState(false);
+   const [_, setLocation] = useLocation();
 
+    const handleOnClick = (applicationId: string) => {
+        setLocation(`${ROUTES.APPLICATIONS}/${applicationId}`);
+    }
     const fastestApplication = findFastestApplication(applications);
     const longestApplication = findLongestApplication(applications);
     const longestPendingApplication = findLongestPendingApplication(applications);
@@ -34,21 +39,21 @@ const Statistics: React.FC<StatisticsProps> = ({ applications }) => {
                        <div className="flex justify-between">
                         <div className="text-gray-200 w-2/5">Fastest Approved:</div>
                         <div className="text-gray-200 ">{fastestApplication.days} days</div>
-                        <GoThereLink link='#'/>
+                        <GoThereLink onClick={() => handleOnClick(fastestApplication.app.id)}/>
                     </div>
                 }
                 {longestApplication &&
                     <div className="flex justify-between">
                         <div className="text-gray-200 w-2/5">Longest Approved:</div>
                         <div className="text-gray-200 ">{longestApplication.days} days</div>
-                        <GoThereLink link='#'/>
+                        <GoThereLink onClick={() => handleOnClick(longestApplication.app.id)}/>
                     </div>
                 }
                 {longestPendingApplication &&
                     <div className="flex justify-between">
                         <div className="text-gray-200 w-2/5">Longest Pending:</div>
                         <div className="text-gray-200 ">{longestPendingApplication.days} days</div>
-                        <GoThereLink link='#'/>
+                        <GoThereLink onClick={() => handleOnClick(longestPendingApplication.app.id)}/>
                     </div>
                 }
                 <Separtor />
