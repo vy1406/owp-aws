9edcd5ba6a2a343e1a7c2dd8962b20f13040512c
Vladimir Elisavetski <yvova88@yahoo.com>
2025-03-13 20:02:11 -0600

    fixing before deploy

--- react/src/services/resource.ts
+++ react/src/services/resource.ts
@@ -2,11 +2,12 @@ import { IResource } from "../utils/types";
 import { API } from "./constants";
 import { rest } from "./rest";
 
+
 export type IResourceData = {
     resources: IResource[];
     services: IResource[];
 }
 
-export const getResources = async (): Promise<IResourceData[] | null> => {
-    return await rest.get<IResourceData[]>(`${API.RESOURCE}/retrieve`);
+export const getResources = async (): Promise<IResourceData | null> => {
+    return await rest.get<IResourceData>(`${API.RESOURCE}/retrieve`);
 };
